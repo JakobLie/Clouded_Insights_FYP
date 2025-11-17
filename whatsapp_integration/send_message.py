ca579dd409dcd550d3ae6d629fc08a022661a202
Jeremy Lin Kairui <jeremylinkairui@gmail.com>
2025-11-18 02:27:36 +0800

    Integrated Whatsapp and Email notification sending into fc_app.py for forecast service

--- whatsapp_integration/send_message.py
+++ whatsapp_integration/send_message.py
@@ -4,14 +4,14 @@ import requests
 
 load_dotenv()
 
-PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+WHATSAPP_PHONE_ID = os.getenv("WHATSAPP_PHONE_ID")
+WHATSAPP_ACCESS_TOKEN = os.getenv("WHATSAPP_ACCESS_TOKEN")
 
 # User must be sent this onboard message first, before any other messages
 def send_whatsapp_onboard_message(to_number):
-    url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
+    url = f"https://graph.facebook.com/v22.0/{WHATSAPP_PHONE_ID}/messages"
     headers = {
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Authorization": f"Bearer {WHATSAPP_ACCESS_TOKEN}",
         "Content-Type": "application/json"
     }
     data = {
@@ -33,29 +33,31 @@ def send_whatsapp_onboard_message(to_number):
         return {"error": str(e)}
 
 # User must have replied to onboard message within 24 hours to be able to receive custom message
-def send_whatsapp_custom_message(to_number, text):
-    url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
+def send_whatsapp_custom_message(phone_number, subject, body):
+    url = f"https://graph.facebook.com/v22.0/{WHATSAPP_PHONE_ID}/messages"
     headers = {
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Authorization": f"Bearer {WHATSAPP_ACCESS_TOKEN}",
         "Content-Type": "application/json"
     }
     data = {
         "messaging_product": "whatsapp",
-        "to": to_number,      # single number
+        "to": phone_number,      # single number
         "type": "text",
-        "text": {"body": text}
+        "text": {"body": f"{subject}\n\n{body}"}
     }
     try:
         response = requests.post(url, headers=headers, json=data)
         response.raise_for_status()
+        print(f" * WhatsApp message sent to {phone_number} successfully!")
         return response.json()
     except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
+        return {f" * Error sending WhatsApp message to {phone_number}: {e}"}
 
-phone_numbers = ["6587654321", "6512345678"]
-message = "This is an automated message from NotificationBot of the CloudedInsights Business Account.\n\nNotifications regarding TSH's predicted PNL Performance will be sent via this conversation."
-
-print(send_whatsapp_custom_message("6596694584", message))
+if __name__ == "__main__":
+    message = "This is an automated message from NotificationBot of the CloudedInsights Business Account.\n\nNotifications regarding TSH's predicted PNL Performance will be sent via this conversation."
+    send_whatsapp_custom_message(
+        to_number = "6596694584", 
+        subject = message)
 
 # for number in phone_numbers:
 #     result = send_whatsapp_custom_message(number, message)
